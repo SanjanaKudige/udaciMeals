e3d60d4fe6da77304776d7acb531bcebcc865c14
Sanjana Kudige Anish Kumar <sanju.ka93@gmail.com>
2019-03-14 23:51:42 -0700

    Add project skeleton

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udaciMealsApp
+ * @description
+ * # udaciMealsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udaciMealsApp', []);
+
